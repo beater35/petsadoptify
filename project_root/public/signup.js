f6f3262720edc34e477d699b9d1a8ae5e95ecfe1
Saugat Pathak <np03cs4a220105@heraldcollege.edu.np>
2024-03-19 12:12:01 +0545

    Signup update

---
+++ project_root/public/signup.js
@@ -0,0 +1,7 @@
+function register() {
+    alert("Register button clicked!"); // You can replace this with your registration functionality
+}
+
+function login() {
+    alert("Login button clicked!"); // You can replace this with your login functionality
+}
