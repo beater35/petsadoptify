6924767d98e43fbc99f5ec75870ea5698a49224f
Shristinaprajapati <shristiprajapati339@gmail.com>
2024-05-05 19:52:19 +0545

    adopt-button-redirect page backend-complete

--- project_root/public/home.js
+++ project_root/public/home.js
@@ -101,6 +101,24 @@ document.addEventListener('DOMContentLoaded', async () => {
             adoptButton.classList.add('adopt-button');
             adoptButton.textContent = 'Adopt Me';
 
+            adoptButton.addEventListener('click', () => {
+                // Retrieve the specific data of the clicked pet
+                const petData = {
+                    name: pet.name,
+                    breed: pet.breed,
+                    gender: pet.gender,
+                    age: pet.age,
+                    description: pet.additionalInfo,
+                    image: pet.image
+                };
+            
+                // Store the pet data in local storage
+                localStorage.setItem('selectedPet', JSON.stringify(petData));
+            
+                  // Redirect the user to another page
+                window.location.href = 'adopt-button-redirect.html'; 
+            });
+            
             // Append the "Adopt Me" button to the pet card container
             petCard.appendChild(petImage);
             petCard.appendChild(petDetails);
