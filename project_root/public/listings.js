1117bd2bab2fea66b1b47bc333718e0479240d17
Shristinaprajapati <shristiprajapati339@gmail.com>
2024-05-05 18:28:15 +0545

    adopt-me-page-

--- project_root/public/listings.js
+++ project_root/public/listings.js
@@ -47,25 +47,25 @@ document.addEventListener('DOMContentLoaded', async () => {
             adoptButton.classList.add('adopt-button');
             adoptButton.textContent = 'Adopt Me';
 
+            
+    adoptButton.addEventListener('click', () => {
+        // Retrieve the specific data of the clicked pet
+        const petData = {
+            name: pet.name,
+            breed: pet.breed,
+            gender: pet.gender,
+            age: pet.age,
+            image: pet.image
+        };
+    
+        // Store the pet data in local storage
+        localStorage.setItem('selectedPet', JSON.stringify(petData));
+    
+        // Redirect the user to another page
+        window.location.href = '/adopt-me-page'; // Replace with the URL of the target page
+    });
 
-            adoptButton.addEventListener('click', () => {
-                // Retrieve the specific data of the clicked pet
-                const petData = {
-                    name: pet.name,
-                    breed: pet.breed,
-                    gender: pet.gender,
-                    age: pet.age,
-                    image: pet.image
-                };
-            
-                // Store the pet data in local storage
-                localStorage.setItem('selectedPet', JSON.stringify(petData));
-            
-                // Redirect the user to another page
-                window.location.href = '/adopt-me-page'; // Replace with the URL of the target page
-            });
 
-            
             // Append the "Adopt Me" button to the pet card container
             petCard.appendChild(petImage);
             petCard.appendChild(petDetails);
@@ -83,6 +83,9 @@ document.addEventListener('DOMContentLoaded', async () => {
     .catch(error => {
         console.error('Error fetching pet data:', error);
     });
+
+
+
 
     document.getElementById("search-btn").addEventListener("click", () => {
       event.preventDefault();
